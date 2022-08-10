49581c9ee8387f525bcc6a8c4466ff2839430996
Eduardo Danna <eduardodanna13@gmail.com>
2022-08-09 21:25:22 -0300

    desafio complementario, aplicando arrays

--- index.js
+++ index.js
@@ -1,3 +1,58 @@
+class servicios {
+    constructor(nombre, plan, costo, id) {
+        this.nombre = nombre;
+        this.plan = plan;
+        this.costo = costo;
+        this.id = id;
+    }
+    sumaIva() {
+        this.costo = this.costo * 1.21;
+    }
+}
+const seguros = [];
+seguros.push(new servicios("Auto", "bronce", 900, 1));
+seguros.push(new servicios("Auto", "plata", 1200, 2));
+seguros.push(new servicios("Auto", "oro", 2500, 3));
+seguros.push(new servicios("Hogar", "bronce", 2000, 4));
+seguros.push(new servicios("Hogar", "plata", 4000, 5));
+seguros.push(new servicios("Hogar", "oro", 6000, 6));
+seguros.push(new servicios("Vida", "bronce", 3000, 7));
+seguros.push(new servicios("Vida", "plata", 4500, 8));
+seguros.push(new servicios("Vida", "oro", 7000, 9));
+alert("planes de seguros de auto 1- bronce, 2- plata, 3- oro");
+alert("planes de seguros de hogar 4- bronce, 5- plata, 6- oro");
+alert("planes de seguros de vida 7- bronce, 8- plata, 9- oro");
+
+for(const servicios of seguros){
+    servicios.sumaIva();
+}
+for(const id of seguros){
+    console.log(id.nombre);
+    console.log(id.plan);
+    console.log(id.costo);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 let nota1 = parseInt(prompt("ingrese primer nota de parcial"));
 let nota2 = parseInt(prompt("ingrese segunda nota de parcial"));
@@ -13,13 +68,67 @@ for (let i = 0; i <= 1; i++){
     } 
 }*/
 
-var servicio1 = 1500;  /*seguro auto*/
-var servicio2 = 2000;  /*seguro hogar*/
-var servicio3 = 3000;  /*seguro vida*/
 
+
+
+
+
+
+
+
+
+
+
+/*
+alert("planes de seguros de auto 1- bronce, 2- plata, 3- oro");
+alert("planes de seguros de hogar 4- bronce, 5- plata, 6- oro");
+alert("planes de seguros de vida 7- bronce, 8- plata, 9- oro");
+for(const costo of precios){
+    console.log(costo);
+    console.log(costo.seguro);
+}
+let id = parseInt(prompt("ingrese el numero del ID de seguro que desee"));
+for(const id of precios){
+    calculoprecio(id, seguro, plan); 
+        if (id == 1){
+            console.log("escogiste el seguro de ", seguro, "y el plan ", plan);
+
+        }else if (id == 2) {
+            console.log("escogiste el seguro de ", seguro, "y el plan ", plan);
+
+        }else if (id == 3) {
+            console.log("escogiste el seguro de ", seguro, "y el plan ", plan);
+
+        }else if (id == 4) {
+            console.log("escogiste el seguro de ", seguro, "y el plan ", plan);
+
+        }else if (id == 5) {
+            console.log("escogiste el seguro de ", seguro, "y el plan ", plan);
+
+        }else if (id == 6) {
+            console.log("escogiste el seguro de ", seguro, "y el plan ", plan);
+
+        }else if (id == 7) {
+            console.log("escogiste el seguro de ", seguro, "y el plan ", plan);
+
+        }else if (id == 8) {
+            console.log("escogiste el seguro de ", seguro, "y el plan ", plan);
+
+        }
+        else if (id == 9) {
+            console.log("escogiste el seguro de ", seguro, "y el plan ", plan);
+
+        }
+}
+alert("el precio de su seguro es $ ", calculoprecio);
+/*
+console.log(servicio1);
+console.log(servicio2);
+console.log(servicio3);
+*//*
 alert("los servicios que ofrecemos son: seguro de auto (numero 1), seguro de hogar (numero 2), seguro de vida (numero 3)")
 let servicioseleccionado = parseInt(prompt("Ingrese el numero del servicio que desea contratar (1, 2 o 3)"));
-
+/*
 function precio(servicio1, servicio2, servicio3,){
     for (let i = 0; i < 10; i++){
         if (servicioseleccionado == 1){
@@ -39,9 +148,9 @@ function precio(servicio1, servicio2, servicio3,){
            servicioseleccionado= parseInt(prompt("Ingrese un numero del 1 al 3 para seleccionar un servicio"));
         }
     }
-   /* return(precio);*/
-}
+   /* return(precio);*//*
+}/*
 precio(servicio1, servicio2, servicio3)
 alert("el precio de su seguro es de $" +servicioseleccionado + " con 21% de iva incluido (seguro hogar y vida)");
-
+*/
 
